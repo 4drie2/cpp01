5f9060f0ffb229bd0ad12665add6d6fd42e738f9
Adrien 42 <adr.bidaux@gmail.com>
2025-10-23 19:59:27 +0200

    ex06 maybe ok

---
+++ ex06/src/Harl.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 18:14:34 by abidaux           #+#    #+#             */
+/*   Updated: 2025/10/23 19:32:42 by abidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {
+	std::cout <<"Tchao (ಠ益ಠ)" << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	const std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*handlers[4])() = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (levels[i] == level)
+		{
+			(this->*handlers[i])();
+			return;
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
+
+void	Harl::debug() {
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info() {
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning() {
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error() {
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
---
+++ ex06/src/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 18:14:37 by abidaux           #+#    #+#             */
+/*   Updated: 2025/10/23 19:58:50 by abidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int get_level_index(const std::string& level) {
+	const std::string   levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; ++i) {
+		if (levels[i] == level) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cerr << "Usage: " << std::endl << av[0] << " <level min>" << std::endl;
+		return 1;
+	}
+
+	Harl harl;
+
+	int level_index = get_level_index(av[1]);
+
+	if (level_index == -1) {
+		std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return 0;
+	}
+
+	switch (level_index) {
+		default:
+			std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+		case 0:
+			harl.complain("DEBUG");
+		case 1:
+			harl.complain("INFO");
+		case 2:
+			harl.complain("WARNING");
+		case 3:
+			harl.complain("ERROR");
+			break;
+	}
+
+	return 0;
+}
